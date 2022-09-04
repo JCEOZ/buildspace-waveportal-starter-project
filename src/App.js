97412be7caf9fa1c1094a7857081a6d00c947f8c
J'CEO'Z <jaroslawzagloba@gmail.com>
2022-09-04 19:48:48 +0200

    added check if wallet is connected

--- src/App.js
+++ src/App.js
@@ -5,8 +5,23 @@ import './App.css';
 export default function App() {
 
   const wave = () => {
-    
+
   }
+
+  const checkIfWalletIsConnected = () => {
+    // First make sure we have access to window.ethereum
+    const { ethereum } = window;
+    if (!ethereum) {
+      console.log("Make sure you have Metamask!");
+    } else {
+      console.log("We have the ethereum object: ", ethereum);
+    }
+  }
+
+  // This runs our function when the page loads.
+  React.useEffect(() => {
+    checkIfWalletIsConnected();
+  }, [])
   
   return (
     <div className="mainContainer">
@@ -17,7 +32,7 @@ export default function App() {
         </div>
 
         <div className="bio">
-        I am farza and I worked on self-driving cars so that's pretty cool right? Connect your Ethereum wallet and wave at me!
+        I am J'CEO'Z and I worked as a developer so that's pretty cool right? Connect your Ethereum wallet and wave at me!
         </div>
 
         <button className="waveButton" onClick={wave}>
